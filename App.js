554360bd6eeafe48d01dfca6712aa19170c85aa4
Eoin O'Kennedy <eoin.okennedy@tudublin.ie>
2024-11-18 15:45:51 +0000

    made a button click

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  function clickMe() {
+    console.log("clicked!");
+    alert("Clicked");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.label}>Enter your firstname</Text>
+      <TextInput style={styles.textbox} placeholder="Enter your firstname"/>
+      <Text>Enter your lastname</Text>
+      <TextInput style={styles.textbox} placeholder="Enter your lastname"/>
+      <Button title="Click Me" onPress={clickMe}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +27,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    fontSize: "25",
+    fontFamily: "Arial"
+  },
+  textbox: {
+    fontSize: "25",
+    fontFamily: "Arial",
+    border: "1px solid black"
+  }
 });
